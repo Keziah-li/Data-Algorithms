d68dcbf216af6396077915ca1d4b104c3afc236f
lijiuhui123 <1519247912@qq.com>
2025-01-11 13:23:37 +0800

    添加几道算法，构建SpringBoot用于测试，熟悉相关知识点

---
+++ dataSimulation/src/test/java/com/data/structure/algorithems/data/DataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data.structure.algorithems.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
