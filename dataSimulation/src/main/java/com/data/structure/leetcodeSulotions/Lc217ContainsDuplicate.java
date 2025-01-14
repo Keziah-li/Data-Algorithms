f6a5e41f155d82256102f6e23c11ee004a249684
lijiuhui123 <1519247912@qq.com>
2025-01-14 09:40:37 +0800

    添加两道算法

---
+++ dataSimulation/src/main/java/com/data/structure/leetcodeSulotions/Lc217ContainsDuplicate.java
@@ -0,0 +1,14 @@
+
+import java.util.HashSet;
+
+public class Lc217ContainsDuplicate {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (!set.add(num)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
